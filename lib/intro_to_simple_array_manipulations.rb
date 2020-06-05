49f611692c3e80dc9d09063fd92cabd0f5711bfc
Netaly Ramirez <21netaly@gmail.com>
2020-06-05 01:43:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,8 @@
 def using_concat (array, array1)
   array.concat(array1)
 end
+
+def using_insert(array, element)
+  # insert takes the index at where to input, new element
+  array.insert(4,element)
+end
